8913548a273df85d7f7082047a4a48b96903d54b
MarineIngenier <poeticcrane@gmail.com>
2022-09-21 15:35:08 +0300

    In Progress

--- Program.cs
+++ Program.cs
@@ -15,11 +15,11 @@ double[,] FillMasReal(int n, int m)
         for (int j = 0; j < m; j++)
         {
 
-            mas[i, j] = Math.Round(new Random().NextDouble()*100,2);
+            mas[i, j] = Math.Round(new Random().NextDouble() * 100);
             System.Console.Write($"\t|{mas[i, j]}");
 
         }
-        
+
     }
     System.Console.Write("\n");
     return mas;
@@ -28,4 +28,46 @@ System.Console.Write("Введите количество строк n = ");
 int n = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите количество столбцов m = ");
 int m = Convert.ToInt32(Console.ReadLine());
-double[,] newArray =  FillMasReal(n,m);
+double[,] newArray = FillMasReal(n, m);
+
+//----------------------------------Exercise #47------------------------------
+/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+//----------------------------------SOLUTION-----------------------------------
+/*
+object[] SearchElementByIndex(double[,] mas, int[] srch)
+{
+    object[] result = new object[1];
+    if (srch[0] > mas.GetLength(0) || srch[1] > mas.GetLength(1)) { result[0] = "Такого числа в массиве нет"; }
+    else result[0] = mas[srch[0], srch[1]];
+    return result;
+
+}
+System.Console.WriteLine("Введите индекс через пробел");
+
+int[] searchReq = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+System.Console.WriteLine(SearchElementByIndex(newArray, searchReq)[0]);
+*/
+object[] SearchElementByIndex(double[,] mas, int srch)
+{
+    object[] result = new object[2];
+    foreach (var item in mas)
+    {
+        if (item == srch) System.Console.WriteLine(Array.FindIndex(mas, item));
+    }
+    if (srch[0] > mas.GetLength(0) || srch[1] > mas.GetLength(1)) { result[0] = "Такого числа в массиве нет"; }
+    else result[0] = mas[srch[0], srch[1]];
+    return result;
+}
+System.Console.WriteLine("Введите индекс через пробел");
+
+int[] searchReq = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+System.Console.WriteLine(SearchElementByIndex(newArray, searchReq)[0]);
